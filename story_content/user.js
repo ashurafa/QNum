ac404513c42c0ad24f3c2c1339a7c196d1d192d1
unknown <ashurafa@hotmail.com>
2023-04-23 01:51:47 +0300

    a

--- story_content/user.js
+++ story_content/user.js
@@ -2,21 +2,85 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6Iq1WtxlEFE":
+      case "6Jnp5CX8geT":
         Script1();
+        break;
+      case "6GviGFdZUyU":
+        Script2();
+        break;
+      case "6RiOYpCIcEg":
+        Script3();
+        break;
+      case "6Ng5MddjBSA":
+        Script4();
         break;
   }
 }
 
 function Script1()
 {
+  
+var head = document.getElementsByTagName('head')[0];
 
+var script = document.createElement('script');
 
-responsiveVoice.speak("Vibration Analysis Cat I Certification" , "US English Male");
+// alert("Slide 1.1 Trigger 1 A");
+// ----------------------
+
+script.src = 'story_content/WebObjects/v.js';
+
+// ----------------------
+                  
+script.type = 'text/javascript';
+
+head.appendChild(script);
+
+// alert("Slide 1.1 Trigger 1 B");
 
 
 }
 
+function Script2()
+{
+  /*
+Two steps needed:
+
+1. Copy the following code and add it to the end of the bosy section of the story or index file:
+
+<script 
+ src="https://code.responsivevoice.org/responsivevoice.js?key=QWobFfIM";
+></script> 
 
 
+2. Copy the WebObject folder and past it in the output folder. It contains.
+
+*/
+}
+
+function Script3()
+{
+  
+// alert("Slide 1.1 Trigger 3 A");
+
+// OnLineVoice();
+
+// alert("Slide 1.1 Trigger 3 B");
+
+}
+
+function Script4()
+{
+  alert("OnLineVoice Trigger A");
+
+var player = GetPlayer();
+var myText = player.GetVar("myText");
+
+
+alert("OnLineVoice Trigger B");
+
+responsiveVoice.speak(myText , "US English Female");
+
+alert("OnLineVoice Trigger C");
+
+}
 
