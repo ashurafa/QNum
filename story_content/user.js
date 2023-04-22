e42f7ad4f8923ab2b888fcfddcf3c268456478dc
unknown <ashurafa@hotmail.com>
2023-04-23 02:01:41 +0300

    x

--- story_content/user.js
+++ story_content/user.js
@@ -2,85 +2,21 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6Jnp5CX8geT":
+      case "6Iq1WtxlEFE":
         Script1();
-        break;
-      case "6GviGFdZUyU":
-        Script2();
-        break;
-      case "6RiOYpCIcEg":
-        Script3();
-        break;
-      case "6Ng5MddjBSA":
-        Script4();
         break;
   }
 }
 
 function Script1()
 {
-  
-var head = document.getElementsByTagName('head')[0];
 
-var script = document.createElement('script');
 
-// alert("Slide 1.1 Trigger 1 A");
-// ----------------------
-
-script.src = 'story_content/WebObjects/v.js';
-
-// ----------------------
-                  
-script.type = 'text/javascript';
-
-head.appendChild(script);
-
-// alert("Slide 1.1 Trigger 1 B");
+responsiveVoice.speak("This tool lets you estimate royalties in a single currency of your choice, based on the current exchange rate. If your book is enrolled in KDP Select, you can use prior KENP Rates to estimate royalties earned from Kindle Unlimited (KU) and you can use prior KENP Rates to estimate KENP royalties.Learn more about the Royalties Estimator." , "US English Male");
 
 
 }
 
-function Script2()
-{
-  /*
-Two steps needed:
-
-1. Copy the following code and add it to the end of the bosy section of the story or index file:
-
-<script 
- src="https://code.responsivevoice.org/responsivevoice.js?key=QWobFfIM";
-></script> 
 
 
-2. Copy the WebObject folder and past it in the output folder. It contains.
-
-*/
-}
-
-function Script3()
-{
-  
-// alert("Slide 1.1 Trigger 3 A");
-
-// OnLineVoice();
-
-// alert("Slide 1.1 Trigger 3 B");
-
-}
-
-function Script4()
-{
-  alert("OnLineVoice Trigger A");
-
-var player = GetPlayer();
-var myText = player.GetVar("myText");
-
-
-alert("OnLineVoice Trigger B");
-
-responsiveVoice.speak(myText , "US English Female");
-
-alert("OnLineVoice Trigger C");
-
-}
 
